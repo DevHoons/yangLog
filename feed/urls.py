fec16c7811e7a57de8bbe3f396b86826b2afcc4b
devhoons <devjhyang@gmail.com>
2020-07-01 16:08:13 +0900

    어드민, 뷰, 모델 뼈대 구축 및 어드민 페이지 접속 에러 수정

--- feed/urls.py
+++ feed/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "feed"
+
 urlpatterns = [
     path("index/", views.index, name="index"),
     # path("/", views.detail, name="detail"),
